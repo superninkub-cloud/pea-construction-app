7a26c7f9da62ef215491d79801b1cf4923056543
unknown <pea@C3PSCPT14.pea.co.th>
2026-08-02 17:55:07 +0700

    fix: remove duplicate layout in personnel page

--- app/personnel/page.tsx
+++ app/personnel/page.tsx
@@ -1,15 +1,7 @@
 "use client";
 
-import ClientLayout from "../components/ClientLayout";
-import AuthWrapper from "../components/AuthWrapper";
 import Personnel from "../components/Personnel";
 
 export default function PersonnelPage() {
-  return (
-    <AuthWrapper>
-      <ClientLayout>
-        <Personnel />
-      </ClientLayout>
-    </AuthWrapper>
-  );
+  return <Personnel />;
 }
